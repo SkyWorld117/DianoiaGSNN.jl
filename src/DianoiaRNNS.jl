189e64c49371144d9ebfba76592c95eaa9d7f3ca
Yi Zhu <46778564+SkyWorld117@users.noreply.github.com>
2021-07-15 00:19:18 +0800

    Update 0.0.2

--- src/DianoiaRNNS.jl
+++ src/DianoiaRNNS.jl
@@ -1,7 +1,11 @@
 module DianoiaRNNS
+    using LinearAlgebra
+    BLAS.set_num_threads(1)
+    
     include("./network.jl")
     include("./flatten.jl")
     include("./oneHot.jl")
+    include("./ga.jl")
 
-    export Network, oneHot, flatten
+    export Network, oneHot, flatten, GA
 end
